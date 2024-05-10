2b0672b82c902475a5a911d3cddbf58b7be728a7
JC-Xevasty <xvstyprogramming@gmail.com>
2024-05-10 13:00:44 +0800

    Updated Helps Page

--- client/src/pages/admin/Help.jsx
+++ client/src/pages/admin/Help.jsx
@@ -1,19 +1,303 @@
+import { NavLink } from "react-router-dom";
 import EmptyState from "../../components/admin/EmptyState";
+import Icon from "../../components/Icon";
+import Input from "../../components/Input";
+import { Fragment, useState } from "react";
 
 const Help = () => {
+  const [search, setSearch] = useState("");
+
+  const TOC = [
+    { id: "navigation", label: "Navigation", hasSubItems: false },
+    { id: "analytics", label: "Analaytics", hasSubItems: false },
+    {
+      id: "trends-map",
+      label: "Trends Map",
+      hasSubItems: true,
+      subItems: [
+        { id: "map", label: "Map" },
+        { id: "list-view", label: "List View" },
+        { id: "upload-data-set", label: "Upload Data Set" },
+      ],
+    },
+    { id: "user-management", label: "User Management", hasSubItems: false },
+    { id: "activity-logs", label: "Activity Logs", hasSubItems: false },
+    { id: "settings", label: "Settings", hasSubItems: false },
+  ];
+
+  const [tocActive, setTOCActive] = useState(false);
+  const [tocAnimate, setTOCAnimate] = useState("");
+
+  const handleAnimationEnd = (e) => {
+    const toc = document.getElementById("toc");
+    if (toc.classList.contains("hide-toc")) {
+      setTOCAnimate("");
+    }
+  };
+
+  const handleClick = () => {
+    setTOCActive(!tocActive);
+    setTOCAnimate(!tocAnimate ? "show-toc" : "hide-toc");
+  };
+
+  const handleSelectSection = (id) => {
+    handleClick();
+    document.getElementById(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
-    <EmptyState
-      iconName="QuestionCircle"
-      heading="Help Content Unavailable"
-      content="We're working on providing instructions in using the system. For immediate assistance, please contact our support team."
-    >
-      <a
-        href={"mailto:" + import.meta.env.VITE_HEALTHPH_EMAIL}
-        className="prod-btn-base prod-btn-primary flex justify-center items-center"
-      >
-        Contact Support
-      </a>
-    </EmptyState>
+    <>
+      <div className="admin-wrapper flex flex-col h-full">
+        <div className="header flex-col sm:flex-row">
+          <div className="breadcrumbs-wrapper me-[16px]">
+            <div className="breadcrumb-item">
+              <NavLink to="/dashboard/help">Help</NavLink>
+              <Icon
+                iconName="ChevronRight"
+                height="16px"
+                width="16px"
+                fill="#A1ACB8"
+                className="icon"
+              />
+            </div>
+          </div>
+          <div className="flex items-start sm:items-center mt-[20px] sm:mt-0 flex-col sm:flex-row">
+            <Input
+              size="input-md"
+              id="search"
+              additionalClasses="w-full max-w-full sm:max-w-[328px]"
+              placeholder="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              leadingIcon="Search"
+              trailingIcon={search.length > 0 ? "Close" : undefined}
+              onClickTrailing={
+                search.length > 0 ? () => setSearch("") : undefined
+              }
+            />
+            <button
+              type="button"
+              className="prod-btn-base prod-btn-secondary flex items-center flex-shrink-0 ms-0 sm:ms-[16px] mt-[16px] sm:mt-0"
+            >
+              <span>Download User Manual</span>
+              <Icon
+                iconName="Download"
+                height="20px"
+                width="20px"
+                fill="#8693A0"
+                className="ms-[8px]"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div className="help-container">
+          <div className={`table-of-contents ${tocAnimate}`} id="toc">
+            <div className="toc-backdrop" onAnimationEnd={handleAnimationEnd}>
+              <div className="close" onClick={handleClick}>
+                <Icon
+                  iconName="Close"
+                  height="30px"
+                  width="30px"
+                  stroke="#FFF"
+                  className="icon"
+                />
+              </div>
+            </div>
+
+            {/* TOC WRAPPER */}
+            <div className="toc-wrapper">
+              <div className="toc-header">Modules</div>
+              <ul>
+                {TOC.map(({ id, label, hasSubItems, subItems }, i) => {
+                  return !hasSubItems ? (
+                    <li
+                      className="toc-item"
+                      key={i}
+                      onClick={() => handleSelectSection(id)}
+                    >
+                      {label}
+                    </li>
+                  ) : (
+                    <Fragment key={i}>
+                      <li
+                        className="toc-item"
+                        onClick={() => handleSelectSection(id)}
+                      >
+                        {label}
+                      </li>
+                      {subItems.map(({ id, label }, i) => {
+                        const isLast = i == subItems.length - 1;
+                        return (
+                          <li
+                            className={`toc-sub-item ${
+                              isLast ? "mb-[16px]" : ""
+                            }`}
+                            key={i}
+                            onClick={() => handleSelectSection(id)}
+                          >
+                            <Icon
+                              iconName={isLast ? "ListEnd" : "List"}
+                              height={isLast ? "31px" : "52px"}
+                              width="10px"
+                              fill="#CCC"
+                              className="icon"
+                            />
+                            <span>{label}</span>
+                          </li>
+                        );
+                      })}
+                    </Fragment>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          {/* TOC Toggler */}
+          <div className="toc-toggler">
+            <div className="toggler-button" onClick={handleClick}>
+              <Icon
+                iconName="UnorderedList"
+                stroke="#000"
+                className="icon"
+                height="24px"
+                width="24px"
+              />
+            </div>
+          </div>
+          {/* HELP CONTENT */}
+          <div className="help-content">
+            {/* NAVIGATION */}
+            <div className="help-content-section" id="navigation">
+              <p className="help-content-heading">Navigation</p>
+              <p className="help-content-desc">
+                The application provides the admin a simple navigation to go
+                through the dashboard. The three mains modules are the
+                Analytics, Trends Map, and User Management. Moreover, the admin
+                can monitor the user activities and manage their personal
+                information in using the application.
+              </p>
+              <SampleImage />
+            </div>
+            {/* ANALYTICS */}
+            <div className="help-content-section" id="analytics">
+              <p className="help-content-heading">Analytics</p>
+              <p className="help-content-desc">
+                Once every user have successfully signed in to HealthPH, the
+                user is directed to the Analytics Page that are consist of
+                summary of data, visualization charts, and many more. The user
+                can filter the data by region and print them in PDF form.
+              </p>
+              <SampleImage />
+            </div>
+            {/* TRENDS MAP */}
+            <div className="help-content-section">
+              <p className="help-content-heading" id="trends-map">
+                Trends Map
+              </p>
+              <p className="help-content-desc">
+                The main module of HealthPH is the Trends Map. The admin is
+                provided with a map of the Philippines to track suspected
+                symptoms in all 17 administrative regions. In the map,
+              </p>
+              <SampleImage />
+              <p className="help-content-subheading" id="map">
+                Map
+              </p>
+              <p className="help-content-desc">
+                By navigating with the map, there are 4 types of colored circles
+                called suspected symptoms. These 4 types of circles are plotted
+                around the map of the Philippines and are categorized as
+                Tuberculosis, Pneumonia, COVID, and AURI. In validating the date
+                of these plotted suspected symptoms, there is a label indicating
+                the recency of the data being displayed.
+              </p>
+              <SampleImage />
+              <p className="help-content-desc">
+                Moreover, the map allows every users to zoom in and out of the
+                map and redirect them to their current location.
+              </p>
+              <SampleImage />
+              <p className="help-content-subheading" id="list-view">
+                List View
+              </p>
+              <p className="help-content-desc">
+                Other than viewing the data using the map, every user can view
+                the data in list view. The list view provides the admin to
+                filter the data by region/s, upload data sets, and view each
+                suspected symptoms in a list.
+              </p>
+              <SampleImage />
+              <p className="help-content-subheading" id="upload-data-set">
+                Upload Data Set
+              </p>
+              <p className="help-content-desc">
+                To continue using the Trends Map with new data, the admin can
+                upload a data set in CSV form and view past uploaded data set on
+                the bottom with a table provided. The admin is also provided
+                with instructions in providing the required data by using the
+                CSV Template located on the top left side.
+              </p>
+              <SampleImage />
+              <p className="help-content-desc">
+                Once the admin have provided the required data using the CSV
+                Template, the admin should proceed in clicking ‘Upload CSV’ then
+                they will be required to do a final check of the CSV data they
+                uploaded by displaying a preview of the table. Once the final
+                check is done, they can proceed again in clicking ‘Upload CSV.’
+                After that, the data set will be updated and be displayed in the
+                Trends Map
+              </p>
+              <SampleImage />
+            </div>
+
+            {/* USER MANAGEMENT */}
+            <div className="help-content-section" id="user-management">
+              <p className="help-content-heading">User Management</p>
+              <p className="help-content-desc">
+                This module provides the admin and super admin to manage its
+                users and themselves. The super admin and admin can add users
+                and manage their status to use the application. However, only
+                the super admin has the privilege to delete users in HealthPH.
+              </p>
+              <SampleImage />
+            </div>
+            {/* ACTIVITY LOGS */}
+            <div className="help-content-section" id="activity-logs">
+              <p className="help-content-heading">Activity Logs</p>
+              <p className="help-content-desc">
+                This module provides every admin to monitor the user activities
+                of all types of users.
+              </p>
+              <SampleImage />
+            </div>
+
+            {/* SETTINGS */}
+            <div className="help-content-section" id="settings">
+              <p className="help-content-heading">Settings</p>
+              <p className="help-content-desc">
+                This module provides every user to edit their personal
+                information, email address, password, and the ability to delete
+                their account. Moreover, every user can check their current
+                status if they can use HealthPH. If a user's status is 'active',
+                they can use the application with their respective privilege and
+                user type. In the case of their status labeled as 'inactive',
+                they are unable to navigate the application can only access the
+                Settings module.
+              </p>
+              <SampleImage />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Help;
+
+const SampleImage = () => {
+  return <div className="sample-image"></div>;
+};
