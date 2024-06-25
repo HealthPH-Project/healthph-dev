411e0e58f86d84636e3e831cddaa76ba455c8292
JC-Xevasty <xvstyprogramming@gmail.com>
2024-06-25 13:58:20 +0800

    Added build.sh

---
+++ server/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Download Chrome
+if [[ ! -d /opt/render/project/.render/chrome ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p /opt/render/project/.render/chrome
+  cd /opt/render/project/.render/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb .
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src
+else
+  echo "...Using Chrome from cache"
+fi
+
+# Install Python libraries (replace with your actual requirements)
+pip install -r requirements.txt
