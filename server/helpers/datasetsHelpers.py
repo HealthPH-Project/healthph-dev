fe2bd12b4900c14feefb4dedb787fe86d72c9495
JC-Xevasty <xvstyprogramming@gmail.com>
2024-07-09 11:33:39 +0800

    Added model

--- server/helpers/datasetsHelpers.py
+++ server/helpers/datasetsHelpers.py
@@ -1,17 +1,15 @@
-import pandas as pd
 import re
-import nltk
 
-nltk.download("wordnet")
+import pandas as pd
 from nltk.stem import WordNetLemmatizer
 from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
-from collections import Counter
-from io import BytesIO
-import random
+
 from geopy.extra.rate_limiter import RateLimiter
 from geopy.geocoders import Nominatim
 from geopy.location import Location
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from packages.skmultilearn.adapt import MLkNN
 
 
 def get_stopwords():
@@ -82,55 +80,58 @@ def get_geopy_latlong(address: str):
     except:
         return "-999, -999"
 
+
 # Get the address details given the latitude and longitude
-def get_geopy_reverse(latlong: str): 
+def get_geopy_reverse(latlong: str):
     if latlong == "-999, -999":
         return {}
-    
+
     geolocator = Nominatim(user_agent="https")
-    
+
     try:
         reverse = RateLimiter(geolocator.reverse, min_delay_seconds=1)
-        
-        location : Location = reverse(latlong, language="en", exactly_one=True)
-        
-        return location.raw['address']
+
+        location: Location = reverse(latlong, language="en", exactly_one=True)
+
+        return location.raw["address"]
     except:
         return {}
-    
+
+
 # Get the PH-Code for region tagging
-def get_PH_code (latlong: str):
+def get_PH_code(latlong: str):
     if latlong == "-999, -999":
         return "N/A"
-    else: 
+    else:
         address = get_geopy_reverse(latlong)
-        
+
         if "ISO3166-2-lvl3" not in address.keys():
             return "N/A"
-        
+
         return address["ISO3166-2-lvl3"]
-    
+
+
 # Check if latitude and longitude coordinates are within in Philippine boundaries
 def is_latlong_within_ph(latlong):
-  # Check if latitude is within PH bounds
-  if latlong[0] < 4.6666667 or latlong[0] > 21.16666667:
+    # Check if latitude is within PH bounds
+    if latlong[0] < 4.6666667 or latlong[0] > 21.16666667:
         return "-999, -999"
 
-  # Check if longitude is within PH bounds
-  if latlong[1] < 116.666666667 or latlong[1] > 126.5666666666:
+    # Check if longitude is within PH bounds
+    if latlong[1] < 116.666666667 or latlong[1] > 126.5666666666:
         return "-999, -999"
 
-  return f"{latlong[0]}, {latlong[1]}"
+    return f"{latlong[0]}, {latlong[1]}"
 
 
-# 
+# Get latitude abd longitude given the province and region
 def filter_location(province, region):
     # Combine province and region for location
     location_str = str(province) + ", " + str(region)
-    
+
     # Get latitude and longitude of location
     latlong = get_geopy_latlong(location_str)
-    
+
     # Check if latitude and longitude is valid
     if latlong == "-999, -999":
         # If not, try getting latitude and longitude for region only
@@ -147,68 +148,50 @@ def filter_location(province, region):
         return is_latlong_within_ph(latlong)
 
 
-def frequent_words(data):
-    # Prepare the post data: concatenate all rows in the post column into a single string
-    post = " ".join(review for review in data.post).lower()
-    # Remove punctuation and numbers
-    post = re.sub(r"[^a-zA-Z\s]", "", post)
-
-    # Define stopwords
-    stopwords = get_stopwords()
-
-    # Split the post into words and filter out stopwords
-    words = [word for word in post.split() if word not in stopwords]
-
-    # Count the frequency of each word
-    word_counts = Counter(words)
-
-    # Get the most common words and their counts
-    most_common_words = word_counts.most_common(
-        10
-    )  # Adjust the number to display more or fewer words
-
-    # Convert the most common words to a DataFrame for visualization
-    df_frequent_words = pd.DataFrame(most_common_words, columns=["Word", "Frequency"])
-
-    return df_frequent_words
+"""
+    BINARY MATRIX
+"""
 
 
-def word_cloud(data, full_path: str):
-    # Combine all posts into a single string
-    post = " ".join(review for review in data.post)
+def get_disease(annotation, disease_code: str):
+    annotation = str(annotation)
 
-    # Define stopwords
-    stopwords = get_stopwords()
+    if annotation == "X" or annotation == "x":
+        return 0
+    else:
+        annotation_splits = str.split(annotation, sep=",")
 
-    # Generate a word cloud image considering stopwords, with higher resolution
-    wordcloud = WordCloud(
-        stopwords=stopwords,
-        background_color="#F8F9FA",
-        font_path="helpers/inter-font.ttf",
-        color_func=wordcloud_color_func,
-        prefer_horizontal=1,
-        max_words=30,
-        random_state=1,
-        width=800,
-        height=400,
-        max_font_size=50,
-        margin=50,
-    ).generate(post)
+        flag = 0
 
-    wordcloud.to_file(full_path)
+        for annotate in annotation_splits:
+            if annotate == disease_code or annotate == f"{disease_code},":
+                flag = 1
+            else:
+                flag = 0 + flag
 
-    return wordcloud
-
-    # return wordcloud
-    image_buffer = BytesIO()
-    wordcloud.to_image().save(image_buffer, format="PNG")
-    image_buffer.seek(0)
-    return image_buffer.getvalue()
+        return flag
 
 
-def wordcloud_color_func(
-    word, font_size, position, orientation, random_state=None, **kwargs
-):
-    colors = ["#171E26", "#007AFF", "#35CA3B", "#DBB324", "#D82727"]
+"""
+    ANNOTATION
+"""
 
-    return random.choice(colors)
+
+def annotate_posts(vectorizer: TfidfVectorizer, mlknn_c: MLkNN, post: str):
+    post_tfidf = vectorizer.transform(post)
+
+    predicted_post_array = mlknn_c.predict(post_tfidf).toarray()
+
+    predicted_post = predicted_post_array[0]
+
+    annotate = []
+
+    annotations = ["AURI", "PN", "TB", "COVID"]
+
+    for i, a in enumerate(predicted_post):
+        if a == 1:
+            annotate.append(annotations[i])
+
+    return annotate if len(annotate) > 0 else "X"
+
+
