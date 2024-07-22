58b186327dc738ad1b352341687146ad078581ab
JC-Xevasty <xvstyprogramming@gmail.com>
2024-07-22 13:55:02 +0800

    test time

--- server/routes/miscRoutes.py
+++ server/routes/miscRoutes.py
@@ -1,6 +1,14 @@
+from datetime import datetime
 from fastapi import APIRouter
 from controllers.miscController import contact_us
 
 router = APIRouter()
 
 router.add_api_route("/contact-us", methods=["POST"], endpoint=contact_us)
+
+
+async def test_time():
+    return {"time": datetime.now()}
+
+
+router.add_api_route("/test-time", methods=["GET"], endpoint=test_time)
