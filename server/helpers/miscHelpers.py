019e8dcc09eaac745eb3b4a57d706d93be4e6742
JC-Xevasty <xvstyprogramming@gmail.com>
2024-07-22 14:23:06 +0800

    Fixed timezone

--- server/helpers/miscHelpers.py
+++ server/helpers/miscHelpers.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 
-async def get_ph_datetime():
+def get_ph_datetime():
     ph_timezone = timezone(timedelta(hours=8))
 
     return datetime.now(ph_timezone)
